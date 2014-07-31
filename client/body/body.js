97ff64ae276e7ba5691dafb2490406393a140a55
Krister Viirsaar <krister.viirsaar@gmail.com>
2014-07-31 19:53:15 +0300

    Sidebar logic

---
+++ client/body/body.js
@@ -0,0 +1,39 @@
+Template.body.events({
+	'mouseover .project': function (e, tmpl) {
+		var total = $('.project').length
+		var index = $(e.currentTarget).index()
+		var colorStep = 360 / total
+		var color = Please.make_color({
+			hue: colorStep * index,
+			golden: false,
+			saturation: .7,
+			value: .7
+		})
+		$(e.currentTarget).css({color: color})
+	},
+	'mouseleave .project': function (e, tmpl) {
+		var total = $('.project').length
+		var index = $(e.currentTarget).index()
+		var colorStep = 360 / total
+		$(e.currentTarget).css({color: 'none'})
+	},
+	'click .project': function (e, tmpl) {
+		var buttonColor = $(e.currentTarget).css('color')
+		$('#extra-wrapper').css({width: '100%', 'background-color': buttonColor})
+		$('.close-extra').css({color: 'white'})
+		Session.set('projectDetails', e.currentTarget.id)
+	},
+	'click .close-extra': function(e, tmpl) {
+		$('#extra-wrapper').css({width: '0%', 'background-color': 'transparent'})
+	}
+})
+
+Template.body.helpers({
+	projectDetails: function() {
+		var tmpl = Session.get('projectDetails')
+		if (isset(tmpl) && isset(Template[tmpl]))
+			return Template[tmpl]
+		else
+			return null
+	}
+})
