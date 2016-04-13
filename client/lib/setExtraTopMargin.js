5f05571414c519ca72448d5d99d56ddd4e72e0ac
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-04-13 13:53:45 +0300

    remove fancy site

--- client/lib/setExtraTopMargin.js
+++
@@ -1,6 +0,0 @@
-setExtraTopMargin = function() {
-	Meteor.setTimeout(function(){
-				var mainTop = $('.title h1').offset().top
-				$('#extra-wrapper .content h1:first-child').css({'margin-top': mainTop})
-			}, 1)
-}
