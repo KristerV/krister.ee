ad311239734be6e2a3fcad62ac62d0a01c73a646
Krister Viirsaar <krister.viirsaar@gmail.com>
2018-07-02 17:15:26 +0300

    Convert menu to list

--- scripts/toc.js
+++ scripts/toc.js
@@ -1,28 +1,41 @@
-let headingsTop = []
-
 // Generate menu
 const allHeadings = document.querySelectorAll('h1, h2, h3')
 const menuDiv = document.querySelector('#tocMenu')
+let lastHeadingNo = 0
+let menuHTML = ''
 allHeadings.forEach(h => {
-    headingsTop.push(h.offsetTop)
-    menuDiv.appendChild(h.cloneNode(true))
+    let currentHeadingNo = parseInt(h.tagName.slice(1))
+    while (currentHeadingNo !== lastHeadingNo) {
+        if (currentHeadingNo > lastHeadingNo) {
+            menuHTML += '<ul>'
+            lastHeadingNo++
+        } else {
+            menuHTML += '</ul>'
+            lastHeadingNo--
+        }
+    }
+    
+    lastHeadingNo = currentHeadingNo
+    menuHTML += `<li>${h.innerHTML}</li>`
 })
+menuDiv.innerHTML = menuHTML
 
 // Update menu on scroll
+let headingsTop = []
+allHeadings.forEach(h => headingsTop.push(h.offsetTop))
 window.onscroll = function() {
-    const scrollPosition = (document.documentElement.scrollTop || document.body.scrollTop) + 60
+    const scrollPosition = (document.documentElement.scrollTop || document.body.scrollTop)
     const activeElements = document.querySelectorAll('#tocMenu .active')
     activeElements.forEach(item => item.classList.remove('active'))
 
     for (var i = 0; i < headingsTop.length; i++) {
-        if (scrollPosition > headingsTop[i] && (!headingsTop[i + 1] || scrollPosition < headingsTop[i+1])) {
-            document.querySelector(`#tocMenu *:nth-child(${i+1})`).classList.add('active')
+        if (scrollPosition > headingsTop[i] && (!headingsTop[i+1] || scrollPosition < headingsTop[i+1])) {
+            document.querySelectorAll(`#tocMenu li`)[i].classList.add('active')
         }
     }
 }
-
 // Scroll on click
-const menuElements = document.querySelectorAll('#tocMenu *')
+const menuElements = document.querySelectorAll('#tocMenu li')
 const headings = document.querySelectorAll(`.rendered-content h1, .rendered-content h2, .rendered-content h3`)
 menuElements.forEach((h, elIndex) => {
     h.addEventListener('click', e => {
